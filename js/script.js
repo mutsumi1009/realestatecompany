5f36a1ec7b081a35e11661e0fe763a3cebd40352
’mutsumi1009’ <moomoo1009@gmail.com>
2025-07-28 04:22:58 +0900

    7/27作成

--- js/script.js
+++ js/script.js
@@ -10,4 +10,12 @@ $(function () {
       cssEase: 'linear'
     });
   });
-  
+  // ナビをクリックしたらスムーススクロール
+$('a[href^="#"]').click(function () {
+  const speed = 600;
+  const href = $(this).attr("href");
+  const target = $(href === "#" || href === "" ? 'html' : href);
+  const position = target.offset().top;
+  $('html, body').animate({ scrollTop: position }, speed, "swing");
+  return false;
+});
